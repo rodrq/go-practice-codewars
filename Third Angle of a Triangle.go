dd6c7eaa7ffa32c7fd71f149bba65ee1de2437b7
Rodri Rooney <rodrqq@gmail.com>
2023-05-05 10:38:35 -0300

    codewars exercises

---
+++ Third Angle of a Triangle.go
@@ -0,0 +1,14 @@
+/* You are given two interior angles (in degrees) of a triangle.
+
+Write a function to return the 3rd.
+
+Note: only positive integers will be tested.
+
+https://en.wikipedia.org/wiki/Triangle
+*/
+
+package kata
+
+func OtherAngle(a int, b int) int {
+	return 180 - a - b
+}
